b06b7d480428f3946367be8f1c3953a4ed653095
shiva singh <shivanand1801@gmail.com>
2025-08-11 18:39:51 +0530

    updated code

--- Parrent_Module/provider.tf
+++ Parrent_Module/provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.38.1"
     }
   }
-  backend "azurerm" {
-    tenant_id            = "7d54cd2e-f350-4121-86cf-50fb446c0bc1"  # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
-    storage_account_name = "shiva_storage1"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
 }
 
 provider "azurerm" {
